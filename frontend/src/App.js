7c6ba24b2e0eb0eb10a4407dea4bdc60c8f6eba1
jibril04Afaa <128831088+jibril04Afaa@users.noreply.github.com>
2025-02-06 03:55:28 -0500

    added tailwind screen sizes

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="border-2 bg-red-500">
+      <p className='border-2 text-center'>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
