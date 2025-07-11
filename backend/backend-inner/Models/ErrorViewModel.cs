5bedd8af833980b3520158b569de19eff755f516
jibril04Afaa <128831088+jibril04Afaa@users.noreply.github.com>
2025-07-10 20:35:47 -0400

    Remove .NET backend and add Java Spring Boot backend

--- backend/backend-inner/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace backend_inner.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
